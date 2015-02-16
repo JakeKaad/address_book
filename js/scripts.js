4b3c8cb91a727e3432ca78c893f2c2e7f23fafff
Alex Beeken and Jake Kaad <kaadalac@gmail.com>
2015-02-16 10:43:09 -0800

    add css styling and finish jquery recording

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,24 @@
 $(function(){
-  $("#contact_new").submit(function() {
+  $("#contact_new").submit(function(event) {
     var inputtedName = $("#contact_name").val();
     var inputtedPhone = $("#contact_phone").val();
     var inputtedEmail = $("#contact_email").val();
-    var newContact = { name: inputtedName,
+    var newContact = { fullName: inputtedName,
                        phone: inputtedPhone,
                        email: inputtedEmail };
 
-    $("#contact_list").append("<li><span class='contact>'" + newContact.name + "</span></li>");
+    $("#contact_list").append("<li><span class='contact'>" + newContact.fullName + "</span></li>");
 
     $("#contact_name").val("");
     $("#contact_phone").val("");
     $("#contact_email").val("");
 
     $(".contact").last().click(function() {
-      $("#card_name").text(newContact.name);
+      $("#card_name").text(newContact.fullName);
       $("#card_phone").text(newContact.phone);
       $("#card_email").text(newContact.email);
     });
+    $("#contact_display").show;
+    event.preventDefault();
   });
 });
